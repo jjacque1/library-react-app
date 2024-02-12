9521f9837b5a2a24d2142239ac30d18d173d6be7
jjacque1 <jacksonjacque.jj@gmail.com>
2024-02-11 21:54:28 -0500

    Creating Navbar => Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav + () => {
+    return (
+        <nav>
+            <div>
+                <a href="/">
+                </a>
+            </div> 
+        </nav>
+    )
+} 
+
+export default Nav;
