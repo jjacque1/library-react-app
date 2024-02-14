d5e52a958da506625e3f68674800f142bbf2652c
jjacque1 <jacksonjacque.jj@gmail.com>
2024-02-13 21:39:40 -0500

    Featured books => section started in React

--- src/components/ui/Highlights.jsx
+++ src/components/ui/Highlights.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Highlights = ({icon}) => {
+const Highlights = ({icon, title, para }) => {
   return (
     <div className="highlight">
       <div className="highlight__img">
         {icon}
       </div>
-      <h3 className="highlight__subtitle">Easy and Quick</h3>
+      <h3 className="highlight__subtitle">{title}</h3>
       <p className="highlight__para">
-        Get access to the book you purchased online istantly.
+       {para}
       </p>
     </div>
   );
