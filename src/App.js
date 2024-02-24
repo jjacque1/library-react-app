4f5ef11b70b175916e7b97867c81daed2f8bb0e2
jjacque1 <jacksonjacque.jj@gmail.com>
2024-02-24 15:58:26 -0500

    Creating Books Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import Landing from './components/Landing';
-import Nav from './components/Nav';
-import Highlights from './components/Highlights'
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
-import Footer from './components/Footer';
-
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Route} from "react-router-dom"
 
 function App() {
-  return <div className="App">
-    <Nav/>
-    <Landing/>
-    <Highlights/>
-    <Featured/>
-    <Discounted/>
-    <Explore/>
-    <Footer/>
-  </div>;
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home}/>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
